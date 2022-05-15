5860c0d33c271d9be497ab2aa128b59e278ba687
xvcrimsajadevx <xvcrimsajadevx@hotmail.com>
2022-05-14 18:31:05 -0600

    Initial commit

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,27 @@
+using RPG.Movement;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                MoveToCursor();
+            }
+        }
+
+        private void MoveToCursor()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(ray, out hit);
+            if (hasHit)
+            {
+                GetComponent<Mover>().MoveTo(hit.point);
+            }
+        }
+    }
+}
