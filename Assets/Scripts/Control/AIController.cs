8d1e3e8fc3cfdea0b360ab4d64877da0ba999c66
xvcrimsajadevx <xvcrimsajadevx@hotmail.com>
2022-05-20 05:01:35 -0600

    Enemies give chase/attack player

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Combat;
+using RPG.Movement;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5;
+
+        Fighter fighter;
+        GameObject player;
+
+        private void Start() {
+            fighter = GetComponent<Fighter>();
+            player = GameObject.FindWithTag("Player");
+        }
+
+        private void Update()
+        {
+            if (InAttackRangeOfPlayer() && fighter.CanAttack(player))
+            {
+                print(gameObject.name + " Should Chase");
+                fighter.Attack(player);
+            }
+            else
+            {
+                fighter.Cancel();
+            }
+        }
+
+        private bool InAttackRangeOfPlayer()
+        {
+            float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+            return distanceToPlayer < chaseDistance;
+        }
+    }
+}
